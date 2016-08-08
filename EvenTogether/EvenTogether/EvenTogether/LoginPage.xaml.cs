0a91a464773d8f05606d4d20d481bb3d0e6e368c
Weuller Marcos Da Silva Santos <wmsantos1@stefanini.com>
2016-08-08 15:28:18 -0300

    Criando projeto

---
+++ EvenTogether/EvenTogether/EvenTogether/LoginPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace EvenTogether
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Login_Clicked(object sender, EventArgs e)
+        {
+            if (txtLogin.Text == "weuller" && txtPassword.Text ==  "123")
+            {
+                App.Current.MainPage = new MainPage();
+            }
+            else
+            {
+                await DisplayAlert("Erro", "Login ou Senha incorretos.", "OK");
+            }
+        }
+
+        void Register_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new RegisterPage();
+        }
+
+    }
+}
