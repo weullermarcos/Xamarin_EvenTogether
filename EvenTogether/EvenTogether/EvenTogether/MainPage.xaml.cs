a83962430d1195225bcf6a131738034f0f47f8c8
wmsantos1 <wmsantos1@STFBSBC032596-L>
2016-08-09 15:02:07 -0300

    Página Inicial

--- EvenTogether/EvenTogether/EvenTogether/MainPage.xaml.cs
+++ EvenTogether/EvenTogether/EvenTogether/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EvenTogether.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,9 +10,47 @@ namespace EvenTogether
 {
     public partial class MainPage : ContentPage
     {
+        private List<Event> _eventList = new List<Event>();
+
         public MainPage()
         {
             InitializeComponent();
+
+            //criando um evento de tapped para a imagem
+            var tapImage = new TapGestureRecognizer();
+            tapImage.Tapped += TapImage_Tapped;
+            imgMenu.GestureRecognizers.Add(tapImage);
+
+            PopulateList();
+
+        }
+
+        private void PopulateList()
+        {
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Event myEvent1 = new Event();
+                myEvent1.Name = "Evento " + i+1;
+                myEvent1.Price = "R$ " + rnd.Next(10, 500) + ",00";
+                myEvent1.Date = DateTime.Now.AddDays(+i);
+                myEvent1.Address = "Local " + i+1;
+                myEvent1.ImageSource = "evento" + rnd.Next(1, 5) + ".png";
+
+                _eventList.Add(myEvent1);
+            }
+
+
+
+            myEventList.ItemsSource = _eventList;
+
+        }
+
+        private void TapImage_Tapped(object sender, EventArgs e)
+        {
+
         }
 
         protected override bool OnBackButtonPressed()
---
+++ EvenTogether/EvenTogether/EvenTogether/Model/Event.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenTogether.Model
+{
+    public class Event
+    {
+        public string Name { get; set; }
+
+        public string Price { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Address { get; set; }
+
+        public string ImageSource { get; set; }
+
+    }
+}
